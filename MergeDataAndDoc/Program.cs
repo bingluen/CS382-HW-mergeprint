eef4f48a1c87439252712a1cb2a09eed21e2dea0
Erickson Juang <a820224@gmail.com>
2016-04-16 01:23:19 +0800

    extract method

--- MergeDataAndDoc/Program.cs
+++ MergeDataAndDoc/Program.cs
@@ -12,14 +12,15 @@ namespace MergeDataAndDoc
     class Program
     {
         private static string data = "", templete = "", output = "";
+        private static List<string> key = new List<string>();
+        private static List<Dictionary<string, string>> inputData = new List<Dictionary<string, string>>();
+
         static void Main(string[] args)
         {
 
             parseArgs(args);
 
-            List<string> key = new List<string>();
-            List<Dictionary<string, string>> inputData = new List<Dictionary<string, string>>();
-
+            
             if (data.Length == 0 || templete.Length == 0 || output.Length == 0)
             {
                 Console.WriteLine("Arguments error! try again");
@@ -28,48 +29,60 @@ namespace MergeDataAndDoc
             {
                 using (StreamReader inputFile = new StreamReader(data))
                 {
-                    // Get column name - key
-                    key.AddRange(inputFile.ReadLine().Split('\t'));
-                    
-                    //Get Data
-                    string tempStream = "";
-                    string[] tempRow;
-                    Dictionary<string, string> tempDataRow;
-                    while ((tempStream = inputFile.ReadLine()) != null)
-                    {
-                        tempDataRow = new Dictionary<string, string>();
-                        tempRow = tempStream.Split('\t');
-                        for (int i = 0; i < tempRow.Length; i++) {
-                            tempDataRow.Add(key[i], tempRow[i]);
-                        }
-                        inputData.Add(tempDataRow);
-                    }
+                    readData(inputFile);
                 }
                 using (StreamReader templeteFile = new StreamReader(templete))
                 using (StreamWriter outputFile = new StreamWriter(output))
                 {
-                    string templeteText = templeteFile.ReadToEnd();
-                    string printStream;
-                    for (int i = 0; i < inputData.Count; i++)
-                    {
-                        printStream = templeteText;
-                        for (int j = 0; j < key.Count; j++)
-                        {
-                            string replacement;
-                            if (inputData[i].TryGetValue(key[j], out replacement)) {
-                                printStream = printStream.Replace("${" + key[j] + "}", replacement);
-                            }
-                        }
-                        Console.WriteLine(printStream);
-                        Console.WriteLine('\n');
-                        outputFile.WriteLine(printStream);
-                        outputFile.WriteLine('\n');
-                    }
+                    mergePrint(templeteFile, outputFile);
                 }
             }
 
  
             Console.ReadLine();
+        }
+
+        private static void mergePrint(StreamReader templeteFile, StreamWriter outputFile)
+        {
+            string templeteText = templeteFile.ReadToEnd();
+            string printStream;
+            for (int i = 0; i < inputData.Count; i++)
+            {
+                printStream = templeteText;
+                for (int j = 0; j < key.Count; j++)
+                {
+                    string replacement;
+                    if (inputData[i].TryGetValue(key[j], out replacement))
+                    {
+                        printStream = printStream.Replace("${" + key[j] + "}", replacement);
+                    }
+                }
+                Console.WriteLine(printStream);
+                Console.WriteLine('\n');
+                outputFile.WriteLine(printStream);
+                outputFile.WriteLine('\n');
+            }
+        }
+
+        private static void readData(StreamReader inputFile)
+        {
+            // Get column name - key
+            key.AddRange(inputFile.ReadLine().Split('\t'));
+
+            //Get Data
+            string tempStream = "";
+            string[] tempRow;
+            Dictionary<string, string> tempDataRow;
+            while ((tempStream = inputFile.ReadLine()) != null)
+            {
+                tempDataRow = new Dictionary<string, string>();
+                tempRow = tempStream.Split('\t');
+                for (int i = 0; i < tempRow.Length; i++)
+                {
+                    tempDataRow.Add(key[i], tempRow[i]);
+                }
+                inputData.Add(tempDataRow);
+            }
         }
         static void parseArgs(string[] args)
         {
